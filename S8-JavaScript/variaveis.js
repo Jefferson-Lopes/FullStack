79da7f6bc072e6c7ee5b7d092478763203730463
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-06-23 20:26:29 -0300

    Create variaveis

---
+++ S8-JavaScript/variaveis.js
@@ -0,0 +1,13 @@
+//String
+var texto = 'Curso de JavaScript'
+
+//Number
+var numeroInt = -7
+var numeroFloat = 10.72
+
+//Boolean
+var test = true
+
+//alert(numeroFloat)
+//document.write(texto)
+console.log()
