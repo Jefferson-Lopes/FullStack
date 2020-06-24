b725cbae4134b9ab70ca0e01849d20b7e608c106
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-06-23 21:20:33 -0300

    Create concatenacao

---
+++ S8-JavaScript/concatenacao.js
@@ -0,0 +1,5 @@
+var nome = prompt('Digite o seu nome: ')
+var idade = prompt('Digite a sua idade: ')
+
+document.write('<h1>Olá ' + nome + ', tudo bem?</h1>')
+document.write('Vi aqui que você tem ' + idade + ' anos!')
