dadff098838cd67116ba8735ebe8f9f943a91208
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-06-23 21:25:29 -0300

    update concatenacao

--- S8-JavaScript/concatenacao.js
+++ S8-JavaScript/concatenacao.js
@@ -1,5 +1,9 @@
 var nome = prompt('Digite o seu nome: ')
 var idade = prompt('Digite a sua idade: ')
+var idade2 = 28
+
+console.log(idade)
+console.log(idade2)
 
 document.write('<h1>Olá ' + nome + ', tudo bem?</h1>')
 document.write('Vi aqui que você tem ' + idade + ' anos!')
