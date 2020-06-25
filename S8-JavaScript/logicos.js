19b9c0a2c2fbb15e7472956e0625345dc5b55eff
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-06-25 14:15:02 -0300

    Create logicos

---
+++ S8-JavaScript/logicos.js
@@ -0,0 +1,19 @@
+/*
+if (2 == 2 && 3 >= 1 || 'a' == 'b'){
+    console.log('True')
+} else {
+    console.log('False')
+}
+*/
+
+var nota = prompt('Digite a sua nota: ')
+var faltas = prompt('Digite a quantidade de faltas: ')
+
+var media = 7
+var faltasMax = 15
+
+if (nota >= media && faltas <= faltasMax){
+    document.write('Aprovado')
+} else {
+    document.write('Reprovado')
+}
