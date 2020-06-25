595129ad83f86946ebc0627a160b7f068b0c704e
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-06-25 13:47:36 -0300

    Create casting

---
+++ S8-JavaScript/casting.js
@@ -0,0 +1,11 @@
+var variavel1 = prompt('Digite um número: ')
+var variavel2 = prompt('Digite outro número: ')
+
+document.write(variavel1 + variavel2 + '<hr>')
+
+variavel1 = parseInt(variavel1)
+variavel2 = parseInt(variavel2)
+
+document.write(variavel1 + variavel2 + '<hr>')
+
+document.write(variavel1.toString() + variavel2.toString())
