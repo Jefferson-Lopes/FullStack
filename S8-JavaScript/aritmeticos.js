773513a501dbacf4eeb3a281c567831a33914ba8
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-06-26 08:29:22 -0300

    Create aritmeticos

---
+++ S8-JavaScript/aritmeticos.js
@@ -0,0 +1,16 @@
+var num1 = 9
+var num2 = 2
+
+document.write('A soma é: ' + (num1 + num2) + '<hr>')
+
+document.write('A subtração é: ' + (num1 - num2) + '<hr>')
+
+document.write('A multiplicação é: ' + (num1 * num2) + '<hr>')
+
+document.write('A divisão é: ' + (num1 / num2) + '<hr>')
+
+document.write('O módulo é: ' + (num1 % num2) + '<hr>')
+
+document.write('O incremento de 9 é: ' + (++num1) + '<hr>')
+
+document.write('O decremento de 10 é: ' + (--num1) + '<hr>')
