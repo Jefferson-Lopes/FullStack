d8d9deff89f4539d2f28d8a5505053eed5f8f09d
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-06-26 08:05:23 -0300

    Create switch

---
+++ S8-JavaScript/switch.js
@@ -0,0 +1,14 @@
+var parametro = 2
+
+switch(parametro){
+    case 1:
+        document.write('Parametro 1')
+        break
+    
+    case 2:
+        document.write('Parametro 2')
+        break
+
+    default:
+        document.write('Default')
+}
