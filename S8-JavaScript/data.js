e92600913f71cf8091517c62b41218423588ef1b
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-07-09 06:43:59 -0300

    Update data.js

--- S8-JavaScript/data.js
+++ S8-JavaScript/data.js
@@ -1,5 +1,27 @@
 var data = new Date()
-
+/*
 var text = data.getDate() + '/' + data.getMonth() + '/' + data.getFullYear()
 
 document.write(text)
+
+
+document.write(data.toString())
+
+data.setDate(data.getDate() + 1)
+
+document.write('<hr>' + data.toString())
+*/
+
+//15/01/2020
+var data1 = new Date(2020, 0, 15)
+
+//23/02/2020
+var data2 = new Date(2020, 1, 23)
+
+document.write(data1.toString())
+document.write('<hr/>' + data2.toString())
+
+document.write('<br><br><hr>')
+
+document.write(data1.getTime())
+document.write('<br>' + data2.getTime())
