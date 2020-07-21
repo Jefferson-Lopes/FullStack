6481b57727a5410e26be47376036a732438fc6e6
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-07-21 07:27:55 -0300

    Update calculadora

--- S8-JavaScript/App1 - calculadora/calculadora.js
+++ S8-JavaScript/App1 - calculadora/calculadora.js
@@ -1,3 +1,24 @@
 function calcular(tipo, valor) {
-    
+    if (tipo === 'acao'){
+
+        if (valor === 'c'){
+            document.getElementById('resultado').value = ''
+        }
+
+        else if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+            document.getElementById('resultado').value += valor
+        }
+
+        else if (valor === '='){
+            var campo = document.getElementById('resultado').value
+            console.log(campo)
+            console.log(eval(campo))
+            document.getElementById('resultado').value = eval(campo)
+        }
+
+
+    }
+    else if (tipo === 'valor'){
+        document.getElementById('resultado').value += valor
+    }
 }
