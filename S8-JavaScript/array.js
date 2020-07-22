6d27a85e72ce016d6d85b7222a688e0f32f0b03e
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-07-22 09:55:55 -0300

    Create array

---
+++ S8-JavaScript/array.js
@@ -0,0 +1,9 @@
+//var lista_frutas = []
+var lista_frutas = Array()
+
+lista_frutas[0] = 'pera'
+lista_frutas[1] = 'maçã'
+lista_frutas[2] = 'melão'
+lista_frutas[3] = 'abacaxi'
+
+console.log(lista_frutas)
