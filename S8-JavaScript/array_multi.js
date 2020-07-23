7e7b04107ccd654ada1f77d6bf0e2ed2be82b5eb
Jefferson Lopes <jeff.777.lopes@gmail.com>
2020-07-23 06:58:21 -0300

    Create array_multi

---
+++ S8-JavaScript/array_multi.js
@@ -0,0 +1,11 @@
+var lista_coisas = Array()
+
+lista_coisas['frutas'] = Array('banana', 'melão')
+
+lista_coisas['pessoas'] = []
+
+lista_coisas['pessoas']['a'] = 'joão'
+lista_coisas['pessoas']['b'] = 'maria'
+lista_coisas['pessoas']['c'] = 'josé'
+
+console.log(lista_coisas)
